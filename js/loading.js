f21ed52e571216fc42c6016e48f32729139444c7
Raphael Feliciano <raphael_feliciano@ymail.com>
2020-02-28 19:00:46 +0800

    added loading screen, modified festival page

---
+++ js/loading.js
@@ -0,0 +1,21 @@
+function onReady(callback) {
+    var intervalID = window.setInterval(checkReady, 3000);
+
+    function checkReady() {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalID);
+            callback.call(this);
+        }
+    }
+}
+
+function show(id, value) {
+    document.getElementById(id).style.display = value ? 'block' : 'none';
+}
+
+onReady(function () {
+    show('header', true);
+    show('content', true);
+    show('footer', true);
+    show('loading', false);
+});
