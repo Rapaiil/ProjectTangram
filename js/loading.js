5e18ec4bae46324cbc024ee584a8d373b7f7b907
Raphael Feliciano <raphael_feliciano@ymail.com>
2020-03-19 21:03:15 +0800

    fixed clapper and navbar

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,5 @@
 function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 8000);
+    var intervalID = window.setInterval(checkReady, 6000);
 
     function checkReady() {
         if (document.getElementsByTagName('body')[0] !== undefined) {
